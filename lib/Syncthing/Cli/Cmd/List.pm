55025583209e983f4db471c8342c53c714d04f91
Vincent <vincent@weborama.com>
2015-04-11 16:27:11 +0400

    improve report

--- lib/Syncthing/Cli/Cmd/List.pm
+++ lib/Syncthing/Cli/Cmd/List.pm
@@ -10,8 +10,15 @@ sub execute {
 	my ($self) = @_;
 	my $config = $self->get('system/config');
 	croak "cant connect to remote" if !scalar keys %$config;
+	my %devices = map { $_->{deviceID} => $_->{name} } @{$config->{devices}};
 	for my $folder(sort { $a->{id} cmp $b->{id} } @{$config->{folders}}) {
-		say $folder->{id}, ': ', $folder->{path};
+		say $folder->{id}, ':';
+		say "    path: ", $folder->{path};
+		say "    devices:";
+		for my $device(sort map { $devices{$_->{deviceID}} // $_->{deviceID} } @{$folder->{devices}}) {
+			say "        * ", $device;
+		}
+		say "";
 	}
 }
 
