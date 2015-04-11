14af6cf473e9d39f91536e24486f433dff06aca5
Vincent <vincent@weborama.com>
2015-04-11 16:52:02 +0400

    add status command

---
+++ lib/Syncthing/Cli/Cmd/Status.pm
@@ -0,0 +1,33 @@
+package Syncthing::Cli::Cmd::Status;
+
+use Moo;
+with 'Syncthing::Cli::Role';
+use MooX::Cmd;
+use feature 'say';
+use Carp;
+use DDP;
+use Number::Bytes::Human qw(format_bytes);
+
+sub execute {
+	my ($self, $args) = @_;
+	my ($directory) = @$args; 
+	croak "Missing directory_id" if !defined $directory;
+	my $config = $self->get('db/status?folder=' . $directory);
+	croak "cant connect to remote" if !scalar keys %$config;
+	croak "nothing found for $directory" if !$config->{version};
+	say $directory,': ';
+	say "    state: ", $config->{state};
+	my $progress = 100;
+	if ($config->{globalBytes}) {
+		$progress = $config->{inSyncBytes} * 100.000 / $config->{globalBytes};
+	}
+	say "    progress: ", sprintf("%.2f", $progress),"%";
+	if ($config->{state} ne 'idle') {
+		say "    fetching: ", $config->{needFiles}, " file(s), ", format_bytes($config->{needBytes}); 
+	}
+	say "    local: ", $config->{localFiles}, " file(s), ", format_bytes($config->{localBytes}); 
+	say "    global: ", $config->{globalFiles}, " file(s), ", format_bytes($config->{globalBytes}); 
+
+}
+
+1;
