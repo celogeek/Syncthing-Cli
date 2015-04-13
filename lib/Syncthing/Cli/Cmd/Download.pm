af31e1f8ee54036c7f3d294a4eed608afc130624
Vincent <vincent@weborama.com>
2015-04-14 01:29:30 +0400

    better display download progress

--- lib/Syncthing/Cli/Cmd/Download.pm
+++ lib/Syncthing/Cli/Cmd/Download.pm
@@ -7,6 +7,7 @@ use feature 'say';
 use Number::Bytes::Human qw(format_bytes);
 use JSON;
 use Term::Screen;
+use Term::ANSIColor qw(:constants);
 use DDP;
 
 sub execute {
@@ -22,38 +23,72 @@ sub execute {
 	my $since = 0;
 	while(1) {
 		last if ($scr->key_pressed && $scr->getch eq 'q');
-		my @events = @{$self->get('events?since='.$since)//[]};
+		my @events =  @{$self->get('events?since='.$since)//[]};
 		next if !@events;
 		$since = $events[-1]->{id};
-
-		my @de = grep {$_->{type} eq 'DownloadProgress'} @events;
-		next if !@de;
-
-		my $pos = 3;
-		my $download = $de[-1]->{data};
-		for my $id(sort keys %$download) {
-			my $files = $download->{$id};
-			for my $file(sort keys %$files) {
-				my $done = $files->{$file}{bytesDone};
-				my $total = $files->{$file}{bytesTotal};
-				my $cols = $scr->cols - 2;
-				my $progress = sprintf("[%-".$cols."s]", "#" x ($total ? int($done / $total * $cols) : $cols));
-
-				$scr
-				->at($pos++,0)->puts(sprintf("%s: %s (%s of %s), %.2f %%", $id, $file,format_bytes($done), format_bytes($total), $total ? $done * 100.000 / $total : 100))->clreol()
-				->at($pos++,0)->puts($progress);
-				$pos++;
+		my @downloadsData = map { $_->{data } } grep { $_->{type} eq 'DownloadProgress' } @events;
+		next if !@downloadsData;
+		my %downloads;
+		for my $download(@downloadsData) {
+			for my $id(keys %$download) {
+				my $files = $download->{$id};
+				for my $file(keys %$files) {
+					my $done = $files->{$file}{bytesDone};
+					my $total = $files->{$file}{bytesTotal};
+					$downloads{$id}{$file} = [$done, $total];
+				}
 			}
 		}
+
+		my $pos = 3;
+		for my $id(sort keys %downloads) {
+			$scr->at($pos++,0)->puts("[" . $id . "]")->clreol();
+			my $files = $downloads{$id};
+			for my $file(sort keys %$files) {
+				$self->display($scr, $pos, $file, $files->{$file});
+				$pos++;
+			}
+			$scr->at($pos++,0)->clreol();
+		}
+
 		while($pos < $scr->cols) {
 			$scr->at($pos++, 0)->clreol;
 		}
 		$scr->at(2,0);
+
 	} continue {
 		sleep(5);
 	}
 	$scr->flush_input;
 	$scr->clrscr();
+}
+
+sub display {
+	my ($self, $scr, $pos, $file, $data) = @_;
+	my ($done, $total) = @$data;
+
+	my $cols = $scr->cols;
+	my $hcols = int($cols / 2);
+	
+	my $progress_str = sprintf("(%s of %s)", format_bytes($done), format_bytes($total));
+	my $max_file_len = $hcols - 3 - length($progress_str);
+	my $file_str = substr($file, -$max_file_len);
+	substr($file_str, 0, 3) = '...' if $file_str ne $file;
+
+	$scr->at($pos,0)->puts(sprintf("%-" . $max_file_len . "s %s", $file_str, $progress_str));
+	
+	my $max_progress_bar_size = $cols - $hcols - 2;
+	my $progress_bar_size = $max_progress_bar_size;
+	$total and
+	$progress_bar_size = int($done / $total * $max_progress_bar_size);
+	
+	$scr
+	->at($pos,$hcols)->puts("[")
+	->at($pos,$hcols+1)->puts(sprintf(ON_WHITE . '%' . $progress_bar_size . 's' . RESET, ''))
+	->at($pos,$cols)->puts("]")
+	;
+	
+
 }
 
 1;
