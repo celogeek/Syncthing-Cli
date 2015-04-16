9454cce70e1fd1544a5b5f8bb23a2317bb75d0df
Vincent <vincent@weborama.com>
2015-04-16 13:13:18 +0400

    update install

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export SYNC_CLI_ROOT="$HOME/.syncthingcli"
+export SYNC_CLI_HOME="$HOME/.syncthingcli"
+export SYNC_CLI_ROOT="$SYNC_CLI_HOME/syncthingcli"
 export PERL_CPANM_OPT="--mirror http://cpan.celogeek.fr -l $SYNC_CLI_ROOT -L $SYNC_CLI_ROOT -nq --self-contained"
 export PERL=/usr/bin/perl
 export CPANM=$SYNC_CLI_ROOT/bin/cpanm
@@ -16,11 +17,10 @@ $PERL $CPANM Dist::Zilla
 dzil authordeps | $PERL $CPANM
 dzil install --install-command="$PERL $CPANM ."
 
-rm -rf $SYNC_CLI_ROOT/bin
-mkdir -p $SYNC_CLI_ROOT/bin
-cat << __EOF__ > $SYNC_CLI_ROOT/bin/syncthing-cli
+mkdir -p $SYNC_CLI_HOME/bin
+cat << __EOF__ > $SYNC_CLI_HOME/bin/syncthing-cli
 #!/bin/bash
-/usr/bin/perl -I$SYNC_CLI_ROOT/lib/perl5 -Mlocal::lib=$SYNC_CLI_ROOT -MSyncthing::Cli -E 'Syncthing::Cli->new_with_cmd' "\$@"
+$SYNC_CLI_ROOT/bin/localenv $SYNC_CLI_ROOT/bin/syncthing-cli "$@"
 __EOF__
 chmod +x $SYNC_CLI_ROOT/bin/syncthing-cli
 
